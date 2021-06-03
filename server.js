e2711ab3a1318a8a84a73d396aa47e2ddd2b485d
jjayjack <jocelynjayjack@gmail.com>
2021-06-03 14:38:22 -0500

    Updated Server and manifest for Heroku

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ if (process.env.NODE_ENV === "production") {
 // Connect to the Mongo DB
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/fourSquare",
-    { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+    { 
+      useUnifiedTopology: true, 
+      useNewUrlParser: true, 
+      useCreateIndex: true,
+      useFindAndModify: false }
   );
   
 
