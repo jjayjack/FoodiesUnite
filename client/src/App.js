d03d8d583cf24cab559d91652cc1312f7367a447
jjayjack <jocelynjayjack@gmail.com>
2021-06-07 11:32:30 -0500

    starting states for FE App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
+
+  const [restaurants, setRestaurants] = useState([]);
+  const [restaurantSearch, setRestaurantSearch] = useState("");
+
+
+
+
   return (
     <Router>
       <div className="container">
         <Switch>
           <Route exact path={"/"}>
+            <NavBar />
             <Homepage />
           </Route>
           <Route exact path = {"/login"}>
