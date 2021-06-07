a3f3ea94c8258881ba29019b4ff2e8108eaf771e
jjayjack <jocelynjayjack@gmail.com>
2021-06-07 15:00:05 -0500

    starting homepage render

---
+++ client/src/components/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <h1>Connected!</h1>
+                       
+        </div>
+    )
+}
+
+export default Homepage
