a3e574eca43a27b446da7de13834d45cc37cb37e
jj espinoza <j.j.espinozagomez@gmail.com>
2021-06-07 12:04:58 -0500

    components

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>P3 app</span>
+    </footer>
+  );
+}
+
+export default Footer;
