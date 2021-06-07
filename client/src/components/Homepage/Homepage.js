76c1420bea1a64a1929179c6f6acb44d814fd1e8
jjayjack <jocelynjayjack@gmail.com>
2021-06-07 16:11:00 -0500

    starting code to grab info from user location

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 const Homepage = () => {
+    const [location, setLocation] = useState("")
+    
+    useEffect(() => {
+        getLocation()
+        
+        return () => {
+            cleanup
+        }
+    }, [input])
+
+    getLocation = () => {
+        navigator.geolocation.getCurrentPosition(res => {
+            console.log(res)
+            setLocation(res.coords.latitude+ "," +res.coords.longitude)
+        });
+    };
+
+    
+
+
     return (
         <div className="homepage">
             <h1>Connected!</h1>
