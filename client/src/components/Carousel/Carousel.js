557c6b5958f78d0131381d9b43e527e3a7d706bd
jjayjack <jocelynjayjack@gmail.com>
2021-06-08 14:12:51 -0500

    Updated carousel

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -1,34 +1,36 @@
-// import React from "react";
-// // import { Link } from "react-router-dom";
-// function Carousel() {
-//     return(
-// <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-//   <ol class="carousel-indicators">
-//     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-//     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-//     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-//   </ol>
-//   <div class="carousel-inner">
-//     <div class="carousel-item active">
-//       <img class="d-block w-100" src="./holders/IMG_3827.jpg" alt="First slide">
-//     </div>
-//     <div class="carousel-item">
-//       <img class="d-block w-100" src="./holders/IMG_3828.jpg" alt="Second slide">
-//     </div>
-//     <div class="carousel-item">
-//       <img class="d-block w-100" src="./holders/IMG_382.jpg" alt="Third slide">
-//     </div>
-//   </div>
-//   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-//     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-//     <span class="sr-only">Previous</span>
-//   </a>
-//   <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-//     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-//     <span class="sr-only">Next</span>
-//   </a>
-// </div>
-//  );
-// }
+import React from "react";
 
-// export default Carousel;
+function Carousel() {
+    return (
+        <>
+            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img className="d-block w-100" src="./holders/IMG_3827.jpg" alt="First slide" />
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src="./holders/IMG_3828.jpg" alt="Second slide" />
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src="./holders/IMG_382.jpg" alt="Third slide" />
+                    </div>
+                </div>
+                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
+        </>
+    );
+}
+
+export default Carousel;
