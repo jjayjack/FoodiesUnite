4640a67d0f8f9e7fec6ec1b559422d8168010349
Justin Lindsey <justinlindseylhr@gmail.com>
2021-06-10 12:46:10 -0500

    updated homepage return

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,4 +1,9 @@
+<<<<<<< Updated upstream
 import React, {useState, useEffect} from 'react'
+=======
+<<<<<<< HEAD
+import React, { useState, useEffect } from 'react'
+>>>>>>> Stashed changes
 import API from '../../utils/API.js'
 import axios from 'axios'
 import Search from "../Search/Search"
@@ -41,5 +46,47 @@ const Homepage = () => {
         </div>
     )
 }
+=======
+import React, { useState, useEffect } from "react";
+import API from "../../utils/API.js";
+import axios from "axios";
+import Search from "../Search/Search";
+import "./index.css";
+// import Carousel from "../Carousel/Carousel"
+
+const Homepage = () => {
+  const [location, setLocation] = useState("");
+  const [venues, setVenues] = useState([]);
+
+  // function getLocation() {
+  //     navigator.geolocation.getCurrentPosition(res => {
+  //         console.log(res);
+  //         setLocation(response.coords.position.latitude+","+response.coords.position.longitude)
+
+  //     }).then(getVenues("taco"))
+  // }
+
+  // useEffect(() => {
+  //     getAPI();
+  //     // getLocation();
+
+  // }, [])
+
+  console.log("Venue state!!!", venues);
+  return (
+    <div className="homepage">
+      <h1>Connected!</h1>
+      <Search setVenues={setVenues} />
+      <ul>
+        {venues.map((venue) => {
+          // return <li key={venue.venue.name}>{venue.venue.name} Location: {venue.venue.location.address}</li>
+          return <h1> {venue.venue.name}</h1>;
+        })}
+      </ul>
+      {/* <Carousel venues={venues} /> */}
+    </div>
+  );
+};
+>>>>>>> b3fd23b50ec74034a336bb3d89f9e42e741c420b
 
 export default Homepage;
